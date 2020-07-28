a762f2f802cc7f3dd7cdd98170e017f36d6577f4
geka <eugene@getsomatic.com>
2020-07-28 17:53:16 +0300

    testing

--- src/node.cc
+++ src/node.cc
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
     }
 
 
-    /*RCLCPP_WARN(log_, "Starting 3");
+    RCLCPP_WARN(log_, "Starting 3");
     auto node3 = std::make_shared<PublisherNode>(3);
     RCLCPP_WARN(log_, "Started 3\n");
     while (!node3->Ready()){
@@ -36,24 +36,23 @@ int main(int argc, char **argv)
     RCLCPP_WARN(log_, "Starting 4");
     auto node4 = std::make_shared<PublisherNode>(4);
     RCLCPP_WARN(log_, "Started 4\n");
-*/
 
     executor.add_node(node1);
     executor.add_node(node2);
-    //executor.add_node(node3);
-    //executor.add_node(node4);
+    executor.add_node(node3);
+    executor.add_node(node4);
 
     rclcpp::on_shutdown([&]{
         RCLCPP_FATAL(log_, "SHUTDOWN");
         executor.cancel();
         executor.remove_node(node1);
         executor.remove_node(node2);
-        /*executor.remove_node(node3);
-        executor.remove_node(node4);*/
+        executor.remove_node(node3);
+        executor.remove_node(node4);
         node1->Stop();
         node2->Stop();
-        /*node3->Stop();
-        node4->Stop();*/
+        node3->Stop();
+        node4->Stop();
     });
 
     executor.spin();
