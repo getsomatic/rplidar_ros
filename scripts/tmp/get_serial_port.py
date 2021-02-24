4c952f2f6ca2b6670a9b4e2071613e2ffe52d145
Alex <olexandr@getsomatic.com>
2021-02-24 16:03:57 +0200

    upd

--- scripts/tmp/get_serial_port.py
+++ scripts/tmp/get_serial_port.py
@@ -7,6 +7,7 @@ import serial.tools.list_ports
 
 def main():
     dev_name = sys.argv[1].strip()
+    print("devname=", dev_name)
     dev_no = 1
     if len(sys.argv) >= 3:
         dev_no = int(sys.argv[2].strip())
@@ -15,6 +16,7 @@ def main():
         if p[1].strip() == dev_name:
             dev_no = dev_no - 1
             if dev_no == 0:
+                print("sys.stdout.write", p[0])
                 sys.stdout.write(p[0])
                 return
 
