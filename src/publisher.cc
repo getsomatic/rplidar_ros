71bc80c9f8bcaf62a4d10fa34f788b69ebe4eb8c
Alex Kovalchuk <olexandr@getsomatic.com>
2021-03-09 15:32:56 +0200

    another fix

--- src/publisher.cc
+++ src/publisher.cc
@@ -15,13 +15,13 @@ PublisherNode::PublisherNode(const std::string & name) : Node("rplidar_" + name)
     timer_ = this->create_wall_timer(20ms, std::bind(&PublisherNode::Tick, this));
 
     if (name == "front_left") {
-        serialPortName_ = "ttyS1";
+        serialPortName_ = "/dev/ttyS1";
     } else if (name == "front_right") {
-        serialPortName_ = "ttyS2";
+        serialPortName_ = "/dev/ttyS2";
     } else if (name == "back_left") {
-        serialPortName_ = "ttyS3";
+        serialPortName_ = "/dev/ttyS3";
     } else if (name == "back_right") {
-        serialPortName_ = "ttyS4";
+        serialPortName_ = "/dev/ttyS4";
     }
 }
 
